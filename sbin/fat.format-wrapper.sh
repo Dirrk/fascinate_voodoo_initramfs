0dc5b95eaa7c7e42bf667e349a1db036f556245c
jt1134 <jt1134@gmail.com>
2010-10-09 17:29:48 -0500

    s/p2/p1

--- sbin/fat.format-wrapper.sh
+++ sbin/fat.format-wrapper.sh
@@ -7,9 +7,9 @@
 PATH=/bin:$PATH
 
 # first : formatting with basic settings we know will always work
-if echo $* | fgrep "/dev/block/mmcblk0p2" ; then
-	echo "Voodoo lagfix: using custom formating for mmcblk0p2"
-	/sbin/fat.format.real "/dev/block/mmcblk0p2"
+if echo $* | fgrep "/dev/block/mmcblk0p1" ; then
+	echo "Voodoo lagfix: using custom formating for mmcblk0p1"
+	/sbin/fat.format.real "/dev/block/mmcblk0p1"
 fi
 
 # second : honor real formatting settings if they really work
